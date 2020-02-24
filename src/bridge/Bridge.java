ba5452123e3b1b4df6f201d6ae19f8d0b4992ac7
David Cosby <60022092@ironschools.org>
2020-02-24 15:03:20 -0700

    Refactored Scene, added renderer and SimulationScene encapsulation

--- src/bridge/Bridge.java
+++ src/bridge/Bridge.java
@@ -1,10 +1,33 @@
 package bridge;
 
-import scene.Scene;
+import scene.SimulationScene;
+import render.Renderer;
 
+/**
+ * @author David Cosby
+ *
+ */
 public class Bridge {
-	public static Scene scene;
+	private static SimulationScene scene;
+	private static Renderer renderer;
 	//public static Scheduler scheduler;
 	//public static UI ui;
+	
+	
+	public static SimulationScene getScene() {
+		return scene;
+	}
+	
+	public static void setScene(SimulationScene newScene) {
+		scene = newScene;
+	}
+	
+	public static Renderer getRenderer() {
+		return renderer;
+	}
+	
+	public static void setRenderer(Renderer newRenderer) {
+		renderer = newRenderer;
+	}
 	
 }
